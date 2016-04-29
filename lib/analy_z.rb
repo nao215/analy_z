29166f21a252d4d25dfee0140ca9bbb4eed99e8c
nao215 <n.yanagisawa215@gmail.com>
2016-04-30 02:37:45 +0900

    hsm tf idf

--- lib/analy_z.rb
+++ lib/analy_z.rb
@@ -11,6 +11,7 @@ module AnalyZ
     attr_accessor :tf
     attr_accessor :idf
     attr_accessor :tf_idf
+    attr_accessor :hsm_tf_idf
     attr_accessor :words
     attr_accessor :texts
     attr_accessor :sentences
@@ -25,27 +26,48 @@ module AnalyZ
 
     def analyze_words sentences, type_ary = ['名詞']
 
-      @texts, @words, @tf, @idf = {}, {}, {}, {}
+      @texts, @words, @tf, @idf, @hsm = {}, {}, {}, {}, {}
 
       sentences.each{|k, sentence| @texts[k] = sentence.map {|s| s[0]}.join }
 
-      sentences.each do |key, sentence|
-        text = sentence.map {|s| s[0] }.join
+      sentences.each do |key, sentence_ary|
+        text = sentence_ary.map {|s| s[0] }.join
         @words[key] = parse_by_natto(text, type_ary)
         @tf[key] = calc_tf(@words[key])
         @idf[key] = calc_idf(@texts, @words[key])
+        @hsm[key] = calc_hsm(@words[key], sentence_ary)
       end
 
       @tf_idf = calc_tf_idf(@tf, @idf)
+      @hsm_tf_idf = calc_hsm_tf_idf(@tf_idf, @hsm)
 
     end
 
     def parse_html html
-      html.gsub(/\"/, '')
-          .split(/<(".*?"|'.*?'|[^'"])*?>|。|！|？|\!|\?/)
-          .map{|s| s.gsub(/\"/, '')}
-          .delete_if{|el| el =~ /\s|\w|\// || el.length <= 2 }
-          .map{|s| [s, 1]}
+      sentences, important_tags = [], []
+      tag_rep = /<(".*?"|'.*?'|[^'"])*?>/
+      h_tag_reg = /<[hH][1-4].*?>.*?<\/[hH][1-4]>/
+
+      important_tags = html.scan(h_tag_reg)
+        .map{|m| [m.gsub(/<(".*?"|'.*?'|[^'"])*?>/, ''), m.match(/[hH][1-4]/)[0] ]}
+
+      sentences = html.gsub(/\"/, '')
+          .split(tag_rep)
+          .delete_if{|el| el != ['h1', 'h2', 'h3', 'h4'] && el =~ /\s/ || el.length <= 1}
+          .map{|m| [m, 1]}
+
+      sentences.each_with_index do |sentence, i|
+        important_tags.each do |tag_data|
+          rate = 2    * 1.75  if tag_data[1] == 'h1'
+          rate = 1.5  * 1.75  if tag_data[1] == 'h2'
+          rate = 1.17 * 1.75  if tag_data[1] == 'h3'
+          rate = 1.17 * 1.75  if tag_data[1] == 'h4'
+          sentences[i][1] = rate if sentence[0].include?(tag_data[0]) || tag_data[0].include?(sentence[0])
+        end
+      end
+
+      sentences
+
     end
 
     def parse_by_natto text, type_ary
@@ -89,6 +111,17 @@ module AnalyZ
       end
     end
 
+    def calc_hsm words, sentence_ary
+      sentence_ary = sentence_ary.select{|sentence| sentence[1] != 1}
+      words.map do |word|
+        rate = 1
+        sentence_ary.each do |sentence|
+          rate = sentence[1] if sentence[0].include?(word[0])
+        end
+        [word, rate]
+      end.uniq
+    end
+
     def calc_tf_idf tf_list_hash, idf_list_hash
 
       tf_idfs = {}
@@ -103,6 +136,24 @@ module AnalyZ
 
       tf_idfs
 
+    end
+
+    def calc_hsm_tf_idf tf_idf_list_hash, hsm
+
+      hsm_tf_idf = {}
+
+      hsm.each do |k, h|
+        hsm[k] = hsm[k].select {|h| h[1] != 1 }
+      end
+
+      tf_idf_list_hash.each do |k, tf_idf_list|
+        hsm_tf_idf[k] = tf_idf_list.map do |tf_idf|
+          rate = hsm[k].assoc(tf_idf[0]) ? hsm[k].assoc(tf_idf[0])[1] : 1
+          [tf_idf[0], tf_idf[1] * rate]
+        end
+      end
+
+      hsm_tf_idf
     end
 
   end
